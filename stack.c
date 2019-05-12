c815c837fe9bff2806a0c49f9bf873d68f83f058
Manu Ravinda <ravindra.manusrivatsa@gmail.com>
2019-05-12 15:45:21 -0500

    Added stack program

--- stack.c
+++ stack.c
@@ -1,64 +1,122 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+// Stack size is always almost known ahead of time
 #define STACKSIZE 5
+int totalnodes=0;
 
+// Create structure for linked list item with one data item. Lets call it Node.
+// These nodes are special in that they can point to the next node and the prev.
 typedef struct linkedlistnode {
-  unsigned int itemnumber;
-  unsigned int value;
-  struct linkedlistnode *nextelement;
-} StackNode;
+  int data;
+  struct linkedlistnode * next;
+  struct linkedlistnode * prev;
+} Node;
 
-// Stack size is known ahead of time,
-StackNode stack[STACKSIZE];
+Node * head, * tail;
 
-// View list function
-int view(StackNode *head){
-  while(head->nextelement != NULL){
-    printf("Item Number: %d Value: %d\n", head->itemnumber, head->value);
-    head= head->nextelement;
+// Function for push stack data
+void push(int inputdata){
+  if(totalnodes == 0){
+    printf("Stack empty. Adding first element.\n");
+    head = (Node * )malloc(sizeof(Node));
+    head->data = inputdata;
+    head->next = NULL; // Ensures untied node is the tail.
+    head->prev = NULL; // Ensures untied node is the head.
+    tail = head; // Match them so that we can access our stack from any point
+    ++totalnodes; // Increment the total node tally
+    return;
   }
-  printf("Item Number: %d Value: %d\n", head->itemnumber, head->value);
-  return 0;
+  else if (totalnodes == STACKSIZE){
+    printf("ERROR: Stack full. Lost traceability. Stack overflow.\n");
+    return;
+  }
+  else {
+    Node * temppointer = tail;
+    tail = (Node * )malloc(sizeof(Node));
+    tail->data = inputdata;
+    tail->next = NULL; // Ensures untied node is the tail.
+    temppointer->next = tail; // Set the old tail to point to the new tail
+    tail->prev = temppointer; // Set the new tail to backtrack to the old tail
+    ++totalnodes; // Increment the total node tally
+    return;
+  }
 }
 
-// Top elment function
-int top(StackNode *head){
-  printf("The top element of the list is %d\n", head->value);
-  return 0;
+// Function to traverse through all the nodes and display them using the head
+void displaynodes(Node * headend){
+  if(totalnodes != 0){
+    printf("List items are ordered as follows:\n");
+    while (headend->next != NULL) {
+      printf("Data: %d\n", headend->data);
+      headend = headend->next;
+    }
+    // Lets not forget the tail
+    printf("Data: %d\n", headend->data);
+    printf("Current stack size: %d\n", totalnodes);
+    printf("----------------------------------\n");
+    return;
+  }
+  printf("Stack empty.\n");
+  return;
 }
 
-// Last element function
-int last(StackNode *head){
-  // Drawbac of the linked list data structure
-  while(head->nextelement != NULL){
-    head = head->nextelement;
+// Function to traverse through all the nodes and display them using the tail
+void displaytailnodes(Node * tailend){
+  if(totalnodes != 0){
+    printf("List items are ordered as follows:\n");
+    while (tailend->prev != NULL) {
+      printf("Data: %d\n", tailend->data);
+      tailend = tailend->prev;
+    }
+    // Lets not forget the head
+    printf("Data: %d\n", tailend->data);
+    printf("Current stack size: %d\n", totalnodes);
+    printf("----------------------------------\n");
+    return;
   }
-  printf("The last element of the list is %d\n", head->value);
-  return 0;
+  printf("Stack empty.\n");
+  return;
 }
 
-int main() {
-  int i=0;
+// Funtion to pop the last value of the stack
+void pop(int * inputdata){
+  Node * temppointer = tail;
+  * inputdata = temppointer->data;
+  tail = temppointer->prev;
+  tail->next=NULL;
+  temppointer->prev = NULL;
+  free(temppointer);
+  --totalnodes;
+  return;
+}
 
-  for (i = 0; i < (LISTSIZE - 1); i++) {
-    list[i].itemnumber = i+1;
-    list[i].value = i*10;
-    list[i].nextelement = &list[i+1];
-  }
+// Main entry point
+int main(void) {
 
-  // Set tail
-  list[i].itemnumber = i+1;
-  list[i].value = i*10;
-  list[i].nextelement = NULL;
+  int data;
 
-  // Viewing a linked list
-  view(list);
+  push(15);
+  displaynodes(head);
 
-  // Top element of the list
-  top(list);
+  push(64);
+  displaynodes(head);
 
-  // Last element of the list
-  last(list);
+  push(12);
+  displaynodes(head);
 
+  push(931);
+  displaynodes(head);
+
+  push(932);
+  displaynodes(head);
+
+  pop(&data);
+  displaytailnodes(tail);
+  printf("Popped data is %d\n", data);
+
+  pop(&data);
+  displaytailnodes(tail);
+  printf("Popped data is %d\n", data);
   return 0;
 }
